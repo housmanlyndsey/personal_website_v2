b25d1cb8de586c188dc5a4b237316bc69de6310c
housmanlyndsey <housmanlyndsey@gmail.com>
2017-03-10 22:26:59 -0600

    third

--- header.php
+++ header.php
@@ -1,26 +1,11 @@
-<nav class="navbar-default" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#home">Lyndsey</a>
-        </div>
-
-        <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
-            <ul class="nav navbar-nav">
-                    
-                <li class="menuItem"><a href="index.php">Home</a></li>
-                <li class="menuItem"><a href="bio.php">About Me </a></li>
-                <li class="menuItem"><a href="gallery.php">Gallery</a></li>
-                <li class="menuItem"><a href="portfolio.php">Portfolio</a></li>                
-                <li class="menuItem"><a href="resume.php">Resume</a></li>
-                <li class="menuItem"><a href="#blog">Blog</a></li>
-                <li class="menuItem"><a href="contact.php">Contact</a></li>
-            </ul>
-        </div> 
-    </div>
-</nav> 
+<nav role="navigation" class="nav turqoise" title="Lyndsey Housman">
+   <ul role="menubar">
+       <li><button>Home</button></li>
+       <li><button>About Me</button></li>
+       <li><button>Gallery</button></li>
+       <li><button>Portfolio</button></li>
+       <li><button>Resume</button></li>
+       <li><button>Blog</button></li>
+       <li><button>Contact</button></li>
+   </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,340 +1,36 @@
- <!-- FlatFy Theme - Andrea Galanti /-->
+
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if IE 9]>    <html class="no-js ie9" lang="en"> <![endif]-->
-<!--[if gt IE 9]><!--> <html> <!--<![endif]-->
+<html>
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
     <meta name="description" content="Lyndsey Housman's Personal Website ">
+    <title> Lyndsey Housman </title>
     <meta name="author" content="Lyndsey Housman">
 
     <title> Lyndsey Housman </title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="plugins/flatfy/css/bootstrap.min.css" rel="stylesheet">
- 
-    <!-- Custom Google Web Font -->
-    <link href="plugins/flatfy/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
-    
-    <!-- Custom CSS-->
-    <link href="plugins/flatfy/css/general.css" rel="stylesheet">
-    <link href="plugins/flatfy/css/custom.css" rel="stylesheet">
-    <link href="plugins/flatfy/css/owl.carousel.css" rel="stylesheet">
-    <link href="plugins/flatfy/css/owl.theme.css" rel="stylesheet">
-    <link href="plugins/flatfy/css/style.css" rel="stylesheet">
-    <link href="plugins/flatfy/css/animate.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">    
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.6.0/css/nanogallery.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.6.0/css/themes/light/nanogallery_light.min.css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">    
-
-    <!-- Magnific Popup core CSS file -->
-    <link rel="stylesheet" href="plugins/flatfy/css/magnific-popup.css"> 
-    
-    <script src="plugins/flatfy/js/modernizr-2.6.2.min.js"></script>  <!-- Modernizr /-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
-    <link href="plugins/ionicons.min.css" rel="stylesheet"/>
-
-    <link rel="stylesheet" href="css/home.css">
-    <link rel="stylesheet" href="css/footer.css">
-
-
+    <!-- Modernizr -->
+    <script src="groundwork/js/libs/modernizr-2.6.2.min.js"></script>
+    <!-- jQuery-->
+    <script type="text/javascript" src="groundwork/js/libs/jquery-1.10.2.min.js"></script>
+    <!-- framework css -->
+    <!--[if gt IE 9]><!-->
+    <link type="text/css" rel="stylesheet" href="groundwork/css/groundwork.css">
+    <!--<![endif]-->
+    <!--[if lte IE 9]>
+    <link type="text/css" rel="stylesheet" href="../css/groundwork-core.css">
+    <link type="text/css" rel="stylesheet" href="../css/groundwork-type.css">
+    <link type="text/css" rel="stylesheet" href="../css/groundwork-ui.css">
+    <link type="text/css" rel="stylesheet" href="../css/groundwork-anim.css">
+    <link type="text/css" rel="stylesheet" href="../css/groundwork-ie.css">
+    <![endif]-->
 </head>
 
-<body id="home">
-
-    <!-- Preloader -->
-    <div id="preloader">
-        <div id="status"></div>
+<body>
+    <div class="container">
+        <?php
+        include('header.php');?>
     </div>
-    
-    <!-- FullScreen -->
-    <div class="intro-header">
-        <div class="col-xs-12 text-center abcen1">
-            <h1 class="h1_home wow fadeIn" data-wow-delay="0.4s">Lyndsey Housman</h1>
-            <h3 class="h3_home wow fadeIn" data-wow-delay="0.6s">Trust me, it's better than my LinkedIn.</h3>
-        </div>    
-        <!-- /.container -->
-        <div class="col-xs-12 text-center abcen wow fadeIn">
-            <div class="button_down "> 
-                <a class="imgcircle wow bounceInUp" data-wow-duration="1.5s"  href="#whatis"> <img class="img_scroll" src="img/icon/circle.png" alt=""> </a>
-            </div>
-        </div>
-    </div>
-    
-    <?php 
-        include('header.php');
-    ?>
-    
-    
-    <!-- Intro -->
-    <div class="content-section-a">
-        <div class="container">
-          <div class="row" >
-             <div class="col-md-6 col-md-offset-3 text-center wrap_title ">
-                <h2>How Can I Help You?</h2>
-            </div>
-        </div>
-            <div class="contentGallery">
-            </div>
-        </div>
-    </div>
-
-    <!-- facts -->
-    <div class="content-section-b"> 
-        
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 wow fadeInLeftBig">
-                     <div id="owl-demo-1" class="owl-carousel">
-                        <a href="plugins/flatfy/img/iphone.png" class="image-link">
-                            <div class="item">
-                                <img  class="img-responsive img-rounded" src="img/photoshoot1.jpg" alt="Me">
-                            </div>
-                        </a>
-                        <a href="plugins/flatfy/img/iphone.png" class="image-link">
-                            <div class="item">
-                                <img  class="img-responsive img-rounded" src="img/AnnArbor-googlemap.png" alt="Ann Arbor map">
-                            </div>
-                        </a>
-                    </div>       
-                </div>
-                
-                <div class="col-sm-6 wow fadeInRightBig"  data-animation-delay="200">   
-                    <h3 class="section-heading">Quick Facts</h3>
-                    <ul class="fa-ul facts">
-                        <li> <i class="fa-li fa fa-female fa-2x"></i> Name: Lyndsey Francyne Housman </li>
-                        <li> <i class="fa-li fa fa-home fa-2x"></i>Hometown: Weston, FL </li>
-                        <li> <i class="fa-li fa fa fa-map-marker fa-2x"></i> Current City: Ann Arbor, MI </li>
-                        <li> <i class="fa-li fa fa-birthday-cake fa-2x"></i>Age: 22 years old </li>
-                    </ul>
-                     <p><a class="btn btn-embossed btn-primary" href="#" role="button">Learn More</a> 
-                     <a class="btn btn-embossed btn-info" href="#" role="button">Back to Top</a></p>
-                </div>              
-            </div>
-        </div>
-    </div>
-
-    <div class="content-section-a" id="education">
-        <div id="educationTransparent">
-            <div class="container-fluid">
-                 <div class="row" id="educationFacts">
-                    <div class="col-sm-6 wow fadeInLeftBig"  data-animation-delay="200">   
-                        <h3> Education</h3>
-                        <div class="col-xs-12 col-md-4 col-lg-4">
-                            <h4> School </h4>
-                            <p> The University of Michigan's Stephen M. Ross School of Business </p>
-                        </div>           
-                        <div class="col-xs-12 col-md-4 col-lg-4">
-                            <h4> Major </h4>
-                            <p> Business Administration (BBA) </p>
-                        </div>
-                        <div class="col-xs-12 col-md-4 col-lg-4">
-                            <h4> Class Standing </h4>
-                            <p> Senior </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row" id="educationFacts">
-                    <div class="col-xs-12 col-sm-6 wow fadeInLeftBig" data-animation-delay:="200">
-                        <h4> Business Courses Taken </h4>
-                        <ul class="list-inline">
-                            <li> Financial Accounting </li>
-                            <li> || </li>
-                            <li> Managerial Accounting </li>
-                            <li> || </li>
-                            <li> Behaviorial Theory in Management </li>
-                            <li> || </li>
-                            <li> Personal Productivity in Information Technology </li>
-                            <li> || </li>
-                            <li> Business Statistics and Management Science </li>
-                            <li> || </li>
-                            <li> Decision Support with Excel </li>
-                            <li> || </li>
-                            <li> Professional Communication Stategies </li>
-                            <li> || </li>
-                            <li> Financial Management </li>
-                            <li> || </li>
-                            <li> Marketing Management I </li>
-                            <li> || </li>
-                            <li> Applied Economics </li>
-                            <li> || </li>
-                            <li> Business Information Systems </li>
-                            <li> || </li>
-                            <li> Operations Management </li>
-                            <li> || </li>
-                            <li> Intro to Entrepreneurship </li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 wow fadeInLeftBig" data-animation-delay:="200">
-                        <h4> Other Notable Courses Taken </h4>
-                        <ul class="list-inline">
-                            <li> Elementary Programming Concepts </li>
-                            <li> || </li>
-                            <li> Agile Web Development: Intro to Web Programming </li>
-                            <li> || </li>
-                            <li> American Constitutional History </li>
-                            <li> || </li>
-                            <li> Naked Eye Astronomy </li>
-                            <li> || </li>
-                            <li> Greek Mythology </li>
-                            <li> || </li>
-                            <li> Intro to International Studies </li>
-                            <li> || </li>
-                            <li> Communications 101: Mass Media </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <!-- Work Experience -->
-    <div id="workExperience" class="content-section-b">
-        <div class="container">
-            <div class="row">
-            <div class="col-md-6 col-md-offset-3 text-center wrap_title">
-                <h2>Work Experience</h2>
-             </div>
-                <div class="col-sm-6  block wow bounceIn">
-                    <div class="row">
-                        <div class="col-md-4 logo"> 
-                            <img src="img/zs-logo.jpg" alt="ZS Associates Logo">
-                        </div>
-                        <div class="col-md-8 box-ct">
-                            <h3> ZS Associates </h3>
-                            <p> <strong> <i class="fa fa-calendar"></i> June 2015-Present </strong> </p>
-                            <p> <i class="fa fa-suitcase"></i> Analyst, Summer Consulting Associate </p>
-                            <p> <i class="fa fa-flag"></i> Evanston, IL </p>
-                        </div>
-                  </div>
-              </div>
-              <div class="col-sm-6 block wow bounceIn">
-                    <div class="row">
-                      <div class="col-md-4 logo"> 
-                            <img src="img/voxy-logo2.png" alt="Voxy Logo"> 
-                      </div>
-                      <div class="col-md-8 box-ct">
-                        <h3> Voxy, Inc </h3>
-                        <p> <strong> <i class="fa fa-calendar"></i> June 2014-May 2015 </strong></p>
-                        <p> <i class="fa fa-suitcase"></i> Consultant and Marketing Analytics Intern </p> 
-                        <p> <i class="fa fa-flag"></i> New York City, NY </p>
-                      </div>
-                    </div>
-              </div>
-          </div>
-          
-          <div class="row tworow">
-                <div class="col-sm-6  block wow bounceIn">
-                    <div class="row">
-                        <div class="col-md-4 logo"> 
-                            <img src="img/counseling-logo.jpg" alt="Counseling Logo">
-                        </div>
-                        <div class="col-md-8 box-ct">
-                            <h3> Advocate Counseling Services </h3>
-                            <p> <strong> <i class="fa fa-calendar"></i> June 2013-August 2013 </strong><p>
-                            <p><i class="fa fa-suitcase"></i> Administrative Assistant</p>
-                            <p><i class="fa fa-flag"></i> Ft. Lauderdale, FL </p>
-                        </div>
-                  </div>
-              </div>
-          </div>
-        </div>
-    </div>
-            <div class="morph-button wow pulse">
-                <button type="button"></button>
-            </div>
-    
-    <?php
-    include("footer.php") ?>
-
-    <!-- JavaScript -->
-    <script src="plugins/flatfy/js/jquery-1.10.2.js"></script>
-    <script src="plugins/flatfy/js/bootstrap.js"></script>
-    <script src="plugins/flatfy/js/owl.carousel.js"></script>
-    <script src="plugins/flatfy/js/script.js"></script>
-    <!-- StikyMenu -->
-    <script src="plugins/flatfy/js/stickUp.min.js"></script>
-    <script type="text/javascript">
-      jQuery(function($) {
-        $(document).ready( function() {
-          $('.navbar-default').stickUp();
-          
-        });
-      });
-        $(document).ready(function() {
-            $('.contentGallery').nanoGallery({
-                thumbnailWidth: 250,
-                thumbnailHeight: 250,
-                items:[
-                    {
-                        src: "img/headshot1.jpg",
-                        title: "Bio",
-                        destURL: 'bio.php'
-                    },
-                    {
-                        src: "img/ENT1.jpg",
-                        title: "Gallery",
-                        destURL: 'gallery.php'
-                    },
-                    {
-                        src: "img/CompetitorAnalysis_Cropped.png",
-                        title: "Portfolio: My Work",
-                        destURL: 'portfolio.php'
-                    },
-                    {
-                        src: "img/Resume_Infographic.png",
-                        title: "Resume",
-                        destURL: 'resume.php'
-                    },
-                    {
-                        src: "img/buddha1.jpg",
-                        title: "My Blog - COMING SOON",
-                        destURL: '#'
-                    },
-                    {
-                        src: "plugins/flatfy/img/icon/letter.svg",
-                        title: "Contact Me",
-                        destURL: 'contact.php'
-                    } 
-                ],
-                thumbnailLabel: {
-                    hideIcons: true,
-                    display: true,
-                    dislpayDescription: true,
-                    postion: 'overImageOnMiddle'
-                },
-                i18n:{
-                    thumbnailImageDescription: "<i style='color:#eee;' class='fa fa-search-plus fa-3x'></i>",
-                    thumbnailAlbumDescription: '<i style="color:#eee;" class="fa fa-search-plus fa-3x"></i>',
-                    breadcrumbHome: '<i class="fa fa-camera fa-1x"></i> Photo albums',
-                    breadcrumbHome_FR: '<i class="fa fa-camera fa-1x"></i> Albums photos'
-                },
-                thumbnailHoverEffect: [{ name: 'labelAppear75', duration: 300 }],
-                theme: 'light',
-                colorScheme: "light",
-                thumbnailLabel: { display: true, position: 'overImageOnMiddle', align: 'center' }
-            });
-        });
-    </script>
-    <!-- Smoothscroll -->
-    <script type="text/javascript" src="plugins/flatfy/js/jquery.corner.js"></script> 
-    <script src="plugins/flatfy/js/wow.min.js"></script>
-    <script>
-     new WOW().init();
-    </script>
-    <script src="plugins/flatfy/js/classie.js"></script>
-    <script src="plugins/flatfy/js/uiMorphingButton_inflow.js"></script>
-    <!-- Magnific Popup core JS file -->
-    <script src="plugins/flatfy/js/jquery.magnific-popup.js"></script> 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.6.0/jquery.nanogallery.min.js"></script> <!-- Nanogallery -->
-</body>
-
 </html>
